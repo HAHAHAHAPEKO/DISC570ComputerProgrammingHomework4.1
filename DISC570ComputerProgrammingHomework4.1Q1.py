bbe94c5a79f8681b12ac51f59e068c8ec14c1177
HAHAHAHAPEKO <2954589@student.dublinisc.com>
2026-02-06 16:43:32 +0000

    Initial commit

--- DISC570ComputerProgrammingHomework4.1Q1.py
+++ DISC570ComputerProgrammingHomework4.1Q1.py
@@ -3,19 +3,19 @@ b = float(input("Enter the coefficient of x: "))
 c = float(input("Enter the constant: "))
 #flag to detect scenarios of the equation
 flag = True
-while a != 0 and b != 0:
+while a != 0:
     while b**2 - (4*a*c) >= 0:
         temp = (b**2 - 4*a*c)**0.5
-        x1 = (-b + temp)/2*a
-        x2 = (-b - temp)/2*a
+        x1 = (-b + temp)/(2*a)
+        x2 = (-b - temp)/(2*a)
         flag = False
-        print(f"The first solution is x1 = {x1}")
-        print(f"The second solution is x2 = {x2}")
+        print(f"The first solution is {x1}")
+        print(f"The second solution is {x2}")
         break
     if flag == True:
-        print("No real roots in your equation")
+        print("No real roots in your equation.")
         flag = False
     break
 if flag == True:
-    print("The coefficient of x^2 and x cannot be 0.")
+    print("The coefficient of x^2 cannot be 0.")
 
